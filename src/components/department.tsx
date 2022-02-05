fcd39c01a8c1455145ee59511b4c9592f0daeccd
Kirubanandan Kanagaraj <anandh.kbn@gmail.com>
2022-02-05 10:15:46 +0100

    Column name Search

--- src/components/department.tsx
+++ src/components/department.tsx
@@ -1,4 +1,5 @@
-import { Form, Table } from 'antd'
+import { SearchOutlined } from '@ant-design/icons'
+import { Button, Form, Input, Space, Table } from 'antd'
 import React, { useState } from 'react'
 import '../App.css'
 import Employees from './employees'
@@ -21,6 +22,85 @@ const Department: React.FC<Props> = ({
   updateCompData,
 }) => {
   const [data, setData] = useState(newDepData)
+  const [searchText, setSearchText] = useState('')
+  const [searchedColumn, setSearchedColumn] = useState('')
+
+  const handleSearch = (selectedKeys, confirm, dataIndex) => {
+    confirm()
+    setSearchText(selectedKeys[0])
+    setSearchedColumn(dataIndex)
+  }
+
+  const handleReset = (clearFilters) => {
+    clearFilters()
+    setSearchText('')
+  }
+
+  const getColumnSearchProps = (dataIndex) => ({
+    filterDropdown: ({
+      setSelectedKeys,
+      selectedKeys,
+      confirm,
+      clearFilters,
+    }) => (
+      <div style={{ padding: 8 }}>
+        <Input
+          placeholder={`Search ${dataIndex}`}
+          value={selectedKeys[0]}
+          onChange={(e) =>
+            setSelectedKeys(e.target.value ? [e.target.value] : [])
+          }
+          onPressEnter={() => handleSearch(selectedKeys, confirm, dataIndex)}
+          style={{ marginBottom: 8, display: 'block' }}
+        />
+        <Space>
+          <Button
+            type="primary"
+            onClick={() => handleSearch(selectedKeys, confirm, dataIndex)}
+            icon={<SearchOutlined />}
+            size="small"
+            style={{ width: 90 }}
+          >
+            Search
+          </Button>
+          <Button
+            onClick={() => handleReset(clearFilters)}
+            size="small"
+            style={{ width: 90 }}
+          >
+            Reset
+          </Button>
+          <Button
+            type="link"
+            size="small"
+            onClick={() => {
+              confirm({ closeDropdown: false })
+              setSearchText(selectedKeys[0])
+              setSearchedColumn(dataIndex)
+            }}
+          >
+            Filter
+          </Button>
+        </Space>
+      </div>
+    ),
+    filterIcon: (filtered) => (
+      <SearchOutlined style={{ color: filtered ? '#1890ff' : undefined }} />
+    ),
+    onFilter: (value, record) =>
+      record[dataIndex]
+        ? record[dataIndex]
+            .toString()
+            .toLowerCase()
+            .includes(value.toLowerCase())
+        : '',
+    // onFilterDropdownVisibleChange: (visible) => {
+    //   if (visible) {
+    //     //setTimeout(() => this.searchInput.select(), 100)
+    //   }
+    // },
+    render: (text) => text,
+  })
 
   const expandedRowRender = (record) => {
     const empObj = record.childData
@@ -31,6 +111,7 @@ const Department: React.FC<Props> = ({
         dataIndex: 'empName',
         key: 'empName',
         editable: true,
+        ...getColumnSearchProps('empName'),
       },
       { title: 'Email', dataIndex: 'email', key: 'email', editable: true },
       { title: 'Designation', dataIndex: 'role', key: 'role', editable: true },
